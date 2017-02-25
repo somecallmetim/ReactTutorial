4608519da2019c75fba470383b95a06e90a99838
somecallmetim <timbauer@ymail.com>
2017-02-25 01:35:04 -0800

    first and final (React Tutorial)

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+import Toolbar from './Toolbar';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (props, { params: {deckId}}) => ({
+    deckId
+});
+
+
+const App = ({ deckId, children}) => {
+    return (
+        <div className='app'>
+            <Toolbar deckId={deckId}/>
+            <Sidebar/>
+            {/*<h1> Deck {deckId} </h1>*/}
+            {children}
+        </div>
+    )
+};
+
+export default connect(mapStateToProps)(App);
